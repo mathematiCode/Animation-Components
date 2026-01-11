b9eea474e26e850b056d0eb31f6518c2e7cba2a9
Julianna Messineo <juliannamessineo@gmail.com>
2026-01-11 16:06:43 -0600

    Add Quadrilateral component

---
+++ animations/src/stories/Quadrilateral/Quadrilateral.jsx
@@ -0,0 +1,24 @@
+function Quadrilateral({}) {
+  return (
+    <svg
+      width="300"
+      height="300"
+      viewBox="0 0 100 100"
+      stroke="black"
+      fill="none"
+      strokeWidth="5"
+    >
+      <path
+        d="
+        M 0,0
+        L 0,100
+        L 100, 100
+        L 100, 0
+        Z
+    "
+      ></path>
+    </svg>
+  );
+}
+
+export default Quadrilateral;
